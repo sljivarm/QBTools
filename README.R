7a1379d71170d55970170ec62d2d8281063b37a9
Maja Sljivar <sljivarm@indiana.edu>
2015-03-06 07:48:34 -0800

    Initial commit

---
+++ README.R
@@ -0,0 +1,14 @@
+############################################################################
+#                                                                           
+#Quantitative Biodiversity Functions Source code
+#
+# Written by: Maja Sljivar
+# 
+#Last update: 2015/03/06
+#
+#Notes: This file contains functions to calculate metrics of taxonomic and 
+# phylogenetic diversity  
+#
+#############################################################################
+
+
